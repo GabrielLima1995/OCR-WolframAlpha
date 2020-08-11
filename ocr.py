6aab06c07b688d06977346bf853db404ce27b901
Gabriel Lima <51869362+GabrielLima1995@users.noreply.github.com>
2020-08-10 22:41:08 -0300

    Ajustes  OCR script / Removido Imagem 0225.jpg - 10/08/2020 22:40 Gabriel Lima

--- ocr.py
+++ ocr.py
@@ -8,12 +8,14 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'< JSON API KEY PATH >'
 client = vision.ImageAnnotatorClient()
 
 image_file = '<Your Image File >'
-folder_path = r'<Folder Path> '
+folder_path = r'<Folder Path>'
+
 
 with io.open(os.path.join(folder_path,image_file),'rb') as img:
     content = img.read()
 
 image = vision.types.Image(content = content)
-response = client.text_detection(image =image)
+response = client.document_text_detection(image =image)
 
-print(type(response))
+text = response.full_text_annotation.text
+print(text)
