09fedccc90790dfae010e14f0ebfb5129f863252
Gabriel Lima <51869362+GabrielLima1995@users.noreply.github.com>
2020-08-10 21:15:38 -0300

    Adicionado Imagens e OCR script - 10/08/2020 21:15 Gabriel Lima

---
+++ ocr.py
@@ -0,0 +1,19 @@
+import base64
+import os 
+import io
+from google.cloud import vision
+from google.cloud.vision import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'< JSON API KEY PATH >'
+client = vision.ImageAnnotatorClient()
+
+image_file = '<Your Image File >'
+folder_path = r'<Folder Path> '
+
+with io.open(os.path.join(folder_path,image_file),'rb') as img:
+    content = img.read()
+
+image = vision.types.Image(content = content)
+response = client.text_detection(image =image)
+
+print(type(response))
